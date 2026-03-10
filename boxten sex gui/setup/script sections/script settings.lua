7998a324f3566ff21c2317d267082a17b84787ac
/ˈɪ-kor-fayj/ <ichorphage@gmail.com>
2026-03-10 00:15:56 -0400

    Update script settings.lua

--- boxten sex gui/setup/script sections/script settings.lua
+++ boxten sex gui/setup/script sections/script settings.lua
@@ -5,7 +5,7 @@
   \ \_\\"\_\  \ \_____\   /\_\/\_\  \ \_\  \ \_____\  \ \_____\  \/\_____\ 
    \/_/ \/_/   \/_____/   \/_/\/_/   \/_/   \/_____/   \/_____/   \/_____/
    
-   Made by Team Noxious -- Boxten Sex GUI [Script Information & File sections]
+   Made by Team Noxious -- Boxten Sex GUI [Script Settings section]
    
 ---------------------------------------------------------------------------------------------------------------------------]]--
 
@@ -47,10 +47,9 @@ end
 
 -------------------------------------------------------------------------------------------------------------------------------
 
-local sections = {
+local section = {
 	version = version,
 
-	["1"] = {
 		{ type = "separator", title = "Script settings" },
 		{ type = "binder", title = "Toggle interface keybind", desc = "Sets the keybind that will toggle the visibility of the interface.", 
 			default = "N",
@@ -108,69 +107,11 @@ local sections = {
 				env.gear.general.encountertwistedblacklist = selected
 			end 
 		}
-	},
-	["2"] = {
-		{ type = "separator", title = "Scaling" },
-		{ type = "slider", title = "Mainframe UI scale", desc = "Adjusts the UI sscale of the mainframe.", min = 0.5, max = 2, default = 1, step = 0.1,
-			callback = function(value)
-				env.gear.general.mainframescale = value
-				ts:Create(env.stuf.mainframescale, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), { Scale = env.gear.general.mainframescale }):Play()
-			end
-		},
-		{ type = "slider", title = "Button UI scale", desc = "Adjusts the UI scale of the buttons.", min = 0.5, max = 2, default = 1, step = 0.1,
-			callback = function(value)
-				env.gear.general.buttonscale = value
-				if env.stuf.setbuttonscale then
-					env.stuf.setbuttonscale(value)
-				end
-			end
-		},
-
-		{ type = "separator", title = "Toons" },
-		{ type = "toggle", title = "Closed captions", desc = "Enables subtitles for the Toons.", 
-			callback = function(state) 
-				env.gear.toons.closedcaptions = state
-			end
-		},
-		{ type = "toggle", title = "Alternate Boxten personality", desc = "Changes Boxten's way of speaking and makes him talk like how he actually talks.", 
-			callback = function(state) 
-				env.gear.toons.nicerboxten = state
-			end
-		},
-		{ type = "button", title = "Switch Boxten with Poppy", desc = "Switches Boxten with Poppy and changes their dialogue.", 
-			callback = function() 
-			end, 
-		},
-		{ type = "button", title = "Switch Poppy with Shrimpo", desc = "Switches Poppy with Shrimpo and changes their dialogue.", 
-			callback = function() 
-			end, 
-		},
-		{ type = "button", title = "Switch Shrimpo with Boxten", desc = "Switches Shrimpo with Boxten and changes their dialogue.", 
-			callback = function() 
-			end, 
-		},
-		{ type = "toggle", title = "Live Poppy reaction", desc = "Allows Poppy to react to your gameplay.", 
-			callback = function(state) 
-				env.gear.toons.livepoppyreaction = state
-			end
-		},
-		{ type = "toggle", title = "Live Shrimpo reaction", desc = "Allows Shrimpo to react to your gameplay.", 
-			callback = function(state) 
-				env.gear.toons.liveshrimporeaction = state
-			end
-		},
-	},
-	["3"] = {
-		{ type = "separator", title = "Current version" },
-		{ type = "label", title = "Version " .. currentversioninfo.version, desc = "Sub-version: " .. currentversioninfo.subversion .. ". Last updated: " .. currentversioninfo.lastupdated, content = currentversioninfo.changelog },
-
-		{ type = "separator", title = "Older versions" },
-		{ type = "label", title = "Version " .. olderversioninfo["1.2.9"].version, desc = "Sub-versions: " .. olderversioninfo["1.2.9"].subversions .. ". Final release: " .. olderversioninfo["1.2.9"].finaldate, content = olderversioninfo["1.2.9"].changelog },
 	}
 }
 
 -------------------------------------------------------------------------------------------------------------------------------
 
-return sections
+return section
 
 -------------------------------------------------------------------------------------------------------------------------------
