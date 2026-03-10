22594b8dd0c310e4f88a69892e2f115da7800162
/ˈɪ-kor-fayj/ <ichorphage@gmail.com>
2026-03-10 00:16:51 -0400

    Update ui settings.lua

--- boxten sex gui/setup/script sections/ui settings.lua
+++ boxten sex gui/setup/script sections/ui settings.lua
@@ -50,66 +50,6 @@ end
 local sections = {
 	version = version,
 
-	["1"] = {
-		{ type = "separator", title = "Script settings" },
-		{ type = "binder", title = "Toggle interface keybind", desc = "Sets the keybind that will toggle the visibility of the interface.", 
-			default = "N",
-			callback = function(key)
-				env.gear.general.defaultkeybind = key
-			end
-		},
-		{ type = "toggle", title = "Debug mode", desc = "Toggles debug mode. Output visible in the Roblox Developer Console.", 
-			default = true,
-			callback = function(state) 
-				env.gear.general.debugmode = state
-			end
-		},
-		{ type = "toggle", title = "Keep on server switch", desc = "Re-executes the script upon switching experiences.", 
-			callback = function(state) 
-				env.gear.general.queueteleport = state
-				queuescript(state)
-			end
-		},
-		{ type = "toggle", title = "Ignore full Research Twisteds", desc = "Ignores every single Twisted in which you already have full Research on when encountering all Twisteds.", 
-			callback = function(state) 
-				env.gear.general.ignoreresearchcompleted = state
-			end
-		},
-		{ type = "slider", title = "Item teleport Y offset", desc = "Sets the Y offset of the target item teleport position (In studs).", min = -5, max = 5, default = -2.5, step = 0.5,
-			callback = function(value)
-				env.gear.general.itemtpposyoffset = value
-			end
-		},
-		{ type = "toggle", title = "Force stop extraction when teleporting to machine", desc = "Forcefully quits machine extraction when teleporting to a random generator.",
-
-			callback = function(state) 
-				env.gear.general.forcequitwhenteleportingtomach = state
-			end
-		},
-
-		{ type = "separator", title = "Blacklists" },
-		{ type = "dropdown", title = "Twisted encounter blacklist", desc = "Blacklists the selected Twisteds from being encountered.", 
-			options = {"Twisted Astro", "Twisted Bassie", "Twisted Blot", "Twisted Bobette", "Twisted Boxten", "Twisted Brightney", "Twisted Brusha",
-				"Twisted Coal", "Twisted Cocoa", "Twisted Connie", "Twisted Cosmo",
-				"Twisted Dandy", "Twisted Dazzle", "Twisted Dyle",
-				"Twisted Eggson", "Twisted Eclipse",
-				"Twisted Finn", "Twisted Flutter", "Twisted Flyte",
-				"Twisted Gigi", "Twisted Ginger", "Twisted Glisten", "Twisted Goob", "Twisted Gourdy",
-				"Twisted Looey",
-				"Twisted Pebble", "Twisted Poppy",
-				"Twisted Razzle", "Twisted Ribecca", "Twisted Rodger", "Twisted Rudie",
-				"Twisted Scraps", "Twisted Shelly", "Twisted Shrimpo", "Twisted Soulvester", "Twisted Sprout",
-				"Twisted Teagan", "Twisted Tisha", "Twisted Toodles",
-				"Twisted Vee",
-				"Twisted Yatta"},
-			multiselect = true,
-
-			callback = function(selected) 
-				env.gear.general.encountertwistedblacklist = selected
-			end 
-		}
-	},
-	["2"] = {
 		{ type = "separator", title = "Scaling" },
 		{ type = "slider", title = "Mainframe UI scale", desc = "Adjusts the UI sscale of the mainframe.", min = 0.5, max = 2, default = 1, step = 0.1,
 			callback = function(value)
@@ -158,15 +98,7 @@ local sections = {
 			callback = function(state) 
 				env.gear.toons.liveshrimporeaction = state
 			end
-		},
-	},
-	["3"] = {
-		{ type = "separator", title = "Current version" },
-		{ type = "label", title = "Version " .. currentversioninfo.version, desc = "Sub-version: " .. currentversioninfo.subversion .. ". Last updated: " .. currentversioninfo.lastupdated, content = currentversioninfo.changelog },
-
-		{ type = "separator", title = "Older versions" },
-		{ type = "label", title = "Version " .. olderversioninfo["1.2.9"].version, desc = "Sub-versions: " .. olderversioninfo["1.2.9"].subversions .. ". Final release: " .. olderversioninfo["1.2.9"].finaldate, content = olderversioninfo["1.2.9"].changelog },
-	}
+		}
 }
 
 -------------------------------------------------------------------------------------------------------------------------------
